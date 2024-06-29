40d2e228062282cdc7159bcc70f94ae0efe599cf
Lizz <lizethjuneidy1902@gmail.com>
2024-06-28 19:51:49 -0500

    nuevo ejersicio

---
+++ EjerciciosDe_Estudio1/Ejercicio1.js
@@ -0,0 +1,23 @@
+
+const plato = {
+    "NombrePlato": "Paella",
+    "precio": 10.000,
+    "Chef":"Lizeth",
+    "ingredientes":[{
+        "nombre":arroz,
+        "cantidad":500,
+        "valor": 2000,
+    }
+    {"nombre": mariscos,
+        "cantidad":400,
+        "precio": 15000,
+    }
+    { "nombre":pollo,
+        "cantidad": 300
+    }
+
+    ],
+    "Acompañantes":[ {
+        "nombre": "papas fritas",
+        "salsa": "aderezo" }]
+}

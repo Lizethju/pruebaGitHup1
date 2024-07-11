22f475efe00214cc2fc7d5602d374fc87ca86568
Lizz <lizethjuneidy1902@gmail.com>
2024-07-11 18:51:55 -0500

    nueva actualización de un ejercicio nuevo

---
+++ EjerciciosDe_Estudio1/Arrays/arraySome.js
@@ -0,0 +1,29 @@
+
+/*const animales=['Perro','Gato','Loro','pez','Tortuga','Elefante'];
+
+/* Alguna de estas palabras tiene más de 7 letras*/
+
+/*const primerCaso= animales.some (function(animales){
+    return animales.length >= 7;
+}
+)
+console.log (primerCaso);
+
+/* Alguna de estas palabras tiene el nombre de el animal conejo*/
+
+/*const segundoCaso = (animales)=> {
+    if(animales.some(conejo=== animales)){
+        console.log('Tiene un conejo');
+    } else (
+        console.error(connejo + 'no existe'))}
+    */ 
+
+        const numero1 = 15
+
+        const numero2 = 26
+        
+        resultado1 = (numero1-numero2);
+
+        
+
+
---
+++ EjerciciosDe_Estudio1/ejercicio resta/resta.js
@@ -0,0 +1,55 @@
+/*const numero1 = prompt("Ingrese el numero" );
+
+let imparpar = numero1 % 2;
+
+if( numero1 % 2 == 0){
+
+    console.log("El numero es par");
+}
+
+else{
+    console.log("El numero es impar");
+}
+    */
+
+/*const num = prompt (" Ingrese el numero");
+
+if(num >= 0){
+    console.log ("El numero es positivo")
+
+}
+else { 
+    console.log(" El numero es negativo")
+}*/
+
+/*let numa= prompt("Ingrese numero1")
+let numb= prompt("Ingrese numero2")
+
+constresultado = (numa / numb);
+
+if( numa == 0 )
+    {
+    console.log("error ingrese otro numero")
+
+    if(numb == 0)
+        {
+        console.log("error")
+    }
+}
+ else {
+    console.log("El resultado es",constresultado)
+}
+*/
+
+let letra = prompt ("Ingre una letra de las opciónes A,B,C,D")
+
+
+
+if (letra == 'a' || letra == 'A'){
+    console.log ("Correcto");
+}
+else {
+    console.log("Error") 
+}
+    
+    

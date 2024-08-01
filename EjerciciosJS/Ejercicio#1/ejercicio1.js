d99fbb9a0bc54132efa82778098319ec277b973d
Lizz <lizethjuneidy1902@gmail.com>
2024-08-01 18:26:26 -0500

    Nueva carpeta

---
+++ EjerciciosJS/Ejercicio#1/ejercicio1.js
@@ -0,0 +1,12 @@
+let productos= [
+{nombre:"Televisor",precio:570.000, descuento:10, total:513000},
+{nombre:"Nevera",precio:1000.000, descuento:20, total:800000},
+{nombre:"Lavadora",precio:680.000, descuento:50, total:340000}
+]
+
+const selecProductos= document.getElementById('selecProducto')
+productos.array.forEach(element => {
+    
+});
+
+console.log(hola)

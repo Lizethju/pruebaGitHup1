477707440387fbf6071d2860901a7b13c42c943f
Lizz <lizethjuneidy1902@gmail.com>
2024-08-02 21:28:21 -0500

    Actualización

--- EjerciciosJS/Ejercicio1/ejercicio1.js
+++ EjerciciosJS/Ejercicio1/ejercicio1.js
@@ -1,13 +1,79 @@
+
 let productos= [
-{nombre:"Televisor",precio:570.000, descuento:10, total:513000},
-{nombre:"Nevera",precio:1000.000, descuento:20, total:800000},
-{nombre:"Lavadora",precio:680.000, descuento:50, total:340000}
+{nombre:"Televisor",precio:570000},
+{nombre:"Nevera",precio:1000000},
+{nombre:"Lavadora",precio:680000}
 ]
 
-const selecProductos= document.getElementById('selecProducto')
-productos.array.forEach(element => {
+let productoSeleccionado= ''
+function seleccionarProducto (producto){
+   productoSeleccionado = producto.options[producto.selectedIndex]
+
+console.log(productoSeleccionado)
+
+}
+
+ function agregarColumna(valor,fila){
+    let nueva=document.createElement('td')
+    nueva.textContent=valor
+    fila.appendChild(nueva)
+}
+
+
+function agregarProducto(){
+   if(productoSeleccionado=== '' || productoSeleccionado.value===1){
+    alert('Debe seleccionar un producto')
+    return
+   }
     
+    const nombreProducto = productoSeleccionado.text
+        const precioProducto = productoSeleccionado.value
+        const descuento =document.getElementById('Descuento').value
+    
+    const subTotal = precioProducto - (precioProducto * descuento / 100) 
+
+    
+console.log(nombreProducto,precioProducto,descuento);
+
+let tablaDeProductos = document.getElementById('tablaProductosTec')
+let nuevaFila=document.createElement('tr')
+agregarColumna(nombreProducto,nuevaFila)
+
+agregarColumna(precioProducto,nuevaFila)
+
+agregarColumna(descuento,nuevaFila)
+
+agregarColumna(subTotal,nuevaFila)
+
+
+tablaDeProductos.appendChild(nuevaFila)
+
+
+    alert(`total selec ${subTotal}`);
+    
+    }
+
+document.addEventListener('DOMContentLoaded', function() {
+
+productos.forEach(element => {
+    const option= document.createElement('option')
+    option.value= element.precio
+    option.text=element.nombre
+    selecProductos.appendChild(option)
 });
 
-console.log(hola)
+})
 
+
+
+
+
+
+/*const descuentoTele = precioProducto * 0.10
+const descuentoNevera = precioProducto * 0.20
+const descuentoLavadora = precioProducto *0.50
+
+const pagarTel = selecProductos - descuentoTele 
+const pagarNev = selecProductos - descuentoNevera
+const pagarLav = selecProductos - descuentoLavadora
+*/
